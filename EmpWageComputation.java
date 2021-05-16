1acb59f0943e4c0f6618f7ec3f2c26156bff6094
karuna17 <karunakakade8@gmail.com>
2021-05-17 01:05:39 +0530

    Added: Check employee present or absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,8 +1,21 @@
 
 public class EmpWageComputation {
 
+	int empCheck = (int) (Math.floor(Math.random()*10)%2);
+	public void empChecking() {
+		if(empCheck==1)
+		{
+			System.out.println("Emloyee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}	
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee wage day-5 program");
+		EmpWageComputation e = new EmpWageComputation();
+		e.empChecking();
 	}
 
 }
