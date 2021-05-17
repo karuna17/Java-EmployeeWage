e01fab18bddb75b23bbc472dfc82b2a457ff8f52
karuna17 <karunakakade8@gmail.com>
2021-05-17 14:05:23 +0530

    Added: Claculate employee wage for diffrent companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,22 +2,28 @@
 public class EmpWageComputation {
 
 	int empCheck = (int) (Math.floor(Math.random()*10)%3);
-	static final int fullTime=1;
-	static final int partTime=2;
-	int ratePerHr=20;
+	static final int fullTime = 1;
+	static final int partTime = 2;
+	String companyName = "";
+	int empHrs = 0;
+	int totalDays;
+	int ratePerHr;
 	int salary;
-	int empHrs=0;
-	static int TotalDays=20;
+
+	public EmpWageComputation(String companyName, int totalDays, int ratePerHr) {
+		this.companyName = companyName;
+		this.totalDays = totalDays;
+		this.ratePerHr = ratePerHr;
+	}
 
 	public void empChecking() {
 		int TotalSalary=0;
 		int day=0;
 		int noOfHrs=0;
-		while(day<TotalDays && noOfHrs<=100)
-		{		
-			int empCheck=(int) (Math.floor(Math.random()*10)%3);
-			switch(empCheck)
-			{
+
+		while(day<totalDays && noOfHrs<100) {		
+			int empCheck=(int) (Math.floor(Math.random()*10)%3);	
+			switch(empCheck) {
 			case fullTime:
 				empHrs=8;
 				salary=empHrs*ratePerHr;
@@ -35,11 +41,15 @@ public class EmpWageComputation {
 		}
 		System.out.println("Total No Of Hrs: "+noOfHrs);
 		System.out.println("Total Employee Wage: "+TotalSalary);
-}
+	}
 
-public static void main(String[] args) {
-	EmpWageComputation e = new EmpWageComputation();
-	e.empChecking();
-}
+	public static void main(String[] args) {
+		EmpWageComputation dmart = new EmpWageComputation("D-Mart",30,20);
+		dmart.empChecking();
+		System.out.println("************************************");
+		EmpWageComputation tata = new EmpWageComputation("TATA",25,30);
+		tata.empChecking();
+
+	}
 
 }
